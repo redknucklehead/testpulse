2cc19f9a12da4ba747fe21f03648db3b963d1b18
Dmitry <radredknucklehead@gmail.com>
2020-12-02 16:09:26 +0300

    untкacked

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// if (2*4 == 8*3) {
+//     console.log('верно')
+// } else {
+//     console.log('неверно')
+// }
+
+// let answer = confirm('Are you 18?');
+// if (answer) {
+//     console.log('Please come on')
+// } else {
+//     console.log('Please leave tjis page!')
+// }
+
+for (let i = 1; i < 8; i++) {
+    console.log(i)
+}
